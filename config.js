035ffd01b44fa0e2087dbde91a8cdf19f31c239c
Ibrahima sory Sacko <ibhex24@gmail.com>
2026-02-05 11:02:37 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,12 @@
-export const config = {
+// config.js
+const config = {
   botName: "IB_HEX",
   ownerName: "IbSacko",
   developer: "ibrahima sory sacko",
   prefix: "Ib",
   mode: "privé",
   version: "2.0",
-  ownerNumber: ["224621963059"] // ton numéro WhatsApp
+  ownerNumber: ["224621963059"] // ton numéro
 };
+
+export default config;

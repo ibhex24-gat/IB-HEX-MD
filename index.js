ef5511d55998bfa01b28fd5d04fd6774629eb25f
Ibrahima sory Sacko <ibhex24@gmail.com>
2026-02-05 02:22:21 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+import { connectBot } from "./lib/connection.js"
+import { handler } from "./lib/handler.js"
 
+const sock = await connectBot()
+
+sock.ev.on("messages.upsert", async ({ messages }) => {
+  const m = messages[0]
+  if (!m.message) return
+  await handler(sock, m)
+})

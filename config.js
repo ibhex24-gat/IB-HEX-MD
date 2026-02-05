0f4f4335c87e7818053d005c38b7a73ff1bc38d9
Ibrahima sory Sacko <ibhex24@gmail.com>
2026-02-05 02:25:58 +0000

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+export const config = {
+  botName: "IB_HEX",
+  ownerName: "IbSacko",
+  developer: "ibrahima sory sacko",
+  prefix: "Ib",
+  mode: "privé",
+  version: "2.0",
+  ownerNumber: ["224XXXXXXXX"] // ton numéro
+}
